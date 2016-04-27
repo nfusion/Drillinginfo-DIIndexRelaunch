f55e5844e54926c859ca0a37b0296fd48732d0b2
Katherine White <kwhite@nfusion.com>
2016-04-27 17:28:05 -0500

    Route cleanup
    Not going to be serving discrete widgets, so I’m removing these
    extraneous routes.

--- app/router.js
+++ app/router.js
@@ -6,18 +6,6 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-  this.route('top-ops');
-  this.route('rig-count');
-  this.route('us-permits');
-  this.route('us-gas-prod-cap');
-  this.route('us-oil-prod-cap');
-  this.route('us-prod-cap');
-  this.route('us-prod-cap-charts');
-  this.route('top-ops-gas');
-  this.route('top-ops-oil');
-  this.route('top-counties-oil');
-  this.route('top-counties-gas');
-});
+Router.map(function() {});
 
 export default Router;
