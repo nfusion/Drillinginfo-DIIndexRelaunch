835d7359ab756026ed76a7709031e501b0f4c91f
Katherine White <kwhite@nfusion.com>
2016-05-06 16:40:14 -0500

    Removing jQuery

--- rig-count/embed.js
+++ rig-count/embed.js
@@ -3,7 +3,7 @@
 	var script_self = document.getElementById('di-embed');
 	var filepath = script_self.src.slice(0,script_self.src.lastIndexOf('/')+1);
 	var frame = document.createElement('iframe');
-	frame.src= filepath + "index.min.html";
+	frame.src= filepath + "index.html";
 	frame.height=200;
 	frame.width=450;
 	frame.style.border=0;
