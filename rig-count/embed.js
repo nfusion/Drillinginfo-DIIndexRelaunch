9b34fbf9e89f44366d70272e9d840f9c5487f6fb
Katherine White <kwhite@nfusion.com>
2016-05-09 11:24:11 -0500

    Self-embedding div
    Cleaner. Script tag is now the only element needed to embed the widget.

--- rig-count/embed.js
+++ rig-count/embed.js
@@ -3,9 +3,10 @@
 	var script_self = document.getElementById('di-embed');
 	var filepath = script_self.src.slice(0,script_self.src.lastIndexOf('/')+1);
 	var frame = document.createElement('iframe');
-	frame.src= filepath + "index.html";
-	frame.height=200;
-	frame.width=450;
-	frame.style.border=0;
-	document.getElementById('drillinginfo').appendChild(frame);
+	frame.id = 'drillinginfo';
+	frame.src = filepath + "index.html";
+	frame.height = 200;
+	frame.width = 450;
+	frame.style.border = 0;
+	script_self.parentNode.insertBefore(frame, script_self.nextSibling);
 })();
