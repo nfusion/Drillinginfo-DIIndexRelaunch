29e6edc10277d543b89b03039cf8445df3324f62
Katherine White <kwhite@nfusion.com>
2016-05-17 11:22:11 -0400

    Applied fixes from StyleCI

--- wp-content/themes/diindex/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/diindex/parts/nav-offcanvas-topbar.php
@@ -4,7 +4,7 @@
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left float-left">
 		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>" class="site-name"><img src="<?php echo get_template_directory_uri (); ?>/assets/images/drillinginfo-logo.png" alt="<?php bloginfo('name'); ?>"></a></li>
+			<li><a href="<?php echo home_url(); ?>" class="site-name"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/drillinginfo-logo.png" alt="<?php bloginfo('name'); ?>"></a></li>
 		</ul>
 	</div>
 	<div class="top-bar-right show-for-medium">
--- wp-content/themes/diindex/parts/nav-topbar.php
+++ wp-content/themes/diindex/parts/nav-topbar.php
@@ -1,7 +1,7 @@
 <div class="top-bar" id="main-menu">
 	<div class="top-bar-left">
 		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>" class="site-name"><img src="<?php echo get_template_directory_uri (); ?>/assets/images/drillinginfo-logo.png" alt="<?php bloginfo('name'); ?>"></a>
+			<li><a href="<?php echo home_url(); ?>" class="site-name"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/drillinginfo-logo.png" alt="<?php bloginfo('name'); ?>"></a>
 			</li>
 		</ul>
 	</div>
