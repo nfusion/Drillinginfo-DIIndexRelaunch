a82160c7b25e860e5b4dcaa05403feaf40113eb3
Katherine White <kwhite@nfusion.com>
2016-05-18 10:46:39 -0500

    Tablet theming

--- wp-content/themes/diindex/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/diindex/parts/nav-offcanvas-topbar.php
@@ -4,7 +4,7 @@
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left float-left">
 		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>" class="site-name"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/drillinginfo-logo.png" alt="<?php bloginfo('name'); ?>"></a></li>
+			<li><a href="<?php echo home_url(); ?>" class="site-name"><div class="logo"><span class="show-for-sr"><?php echo get_bloginfo('name'); ?></span></div></a></li>
 		</ul>
 	</div>
 	<div class="top-bar-right show-for-medium">
@@ -25,11 +25,11 @@
 	<div class="float-right">
 	<?php 
         $args = array(
-        'menu'    => 'Primary Navigation',
-        'submenu' => 'DI Index',
-    );
+	        'menu'    => 'Primary Navigation',
+	        'submenu' => 'DI Index',
+	    );
 
-    wp_nav_menu($args);
+	    wp_nav_menu($args);
     ?>
 </div>
 </div>
