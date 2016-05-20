862ec45f5f1973bda09ab99a5094e4cbb61a5d9e
Katherine White <kwhite@nfusion.com>
2016-05-19 21:03:38 -0500

    Vendor prefixes

--- wp-content/plugins/diindex/app/ember-cli-build.js
+++ wp-content/plugins/diindex/app/ember-cli-build.js
@@ -37,7 +37,10 @@ module.exports = function(defaults) {
   app.import('bower_components/bourbon/app/assets/stylesheets/_bourbon.scss');
   //app.import('bower_components/foundation-sites/scss/foundation.scss');
   //app.import('bower_components/foundation-sites/scss/util/_util.scss');
-  app.import('bower_components/foundation-sites/dist/foundation.js');
+  app.import({
+    'development': 'bower_components/foundation-sites/dist/foundation.js',
+    'test': 'bower_components/foundation-sites/dist/foundation.js',
+  });
 
   return app.toTree();
 };
